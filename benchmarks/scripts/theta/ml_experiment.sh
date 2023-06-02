67c0889688f8178f6775c9b933b2b887b97ce7ca
Alok Kamatar <alokvk2@uchicago.edu>
2023-06-02 14:48:06 -0500

    Creating script to run ml experiment

---
+++ benchmarks/scripts/theta/ml_experiment.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+cd /lus/swift/home/alokvk2/lazy-imports
+source setup.sh
+export BLOCKSIZE=$COBALT_BLOCKSIZE
+
+for nodes in 64 32 16 8 4 2 1; do
+	for workers_per_node in 1 2 4 8 16 32 64; do
+	    workers=$((${workers_per_node} * ${nodes}))
+		if [ workers -ge 64  ] && [ workers -le 2048 ]; then
+			for method in "lazy" "file_system"; do
+				echo "Running ${method} nodes ${nodes}, workers per node ${workers_per_node}"
+				python benchmarks/ml_inference.py \
+					--workers ${workers} \
+					--nodes ${nodes} \
+					--method ${method} \
+					--output results/theta/ml_results.jsonl
+			done
+			rm /lus/sqift/home/alokvk2/.proxy_modules/module-store/*
+			rm argument_store/*
+		fi
+    done
+done
